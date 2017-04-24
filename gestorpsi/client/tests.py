ee1a169200840a86cc10b84f50915c53df9a4265
Dylan Guedes <djmgguedes@gmail.com>
2017-04-24 10:49:43 -0300

    Rewritten routes to use url methods, new client tests
    Signed-off-by: Dylan Guedes <djmgguedes@gmail.com>

--- gestorpsi/client/tests.py
+++ gestorpsi/client/tests.py
@@ -13,16 +13,41 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 """
+from django.test import TestCase, RequestFactory
+from django.test import Client as Cl
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User, UserManager, Group
 
 from gestorpsi.client.models import Client, ClientManager
 from gestorpsi.client.models import Person
 from gestorpsi.phone.models import Phone
-# from gestorpsi.organization.models  import Organization
+from gestorpsi.util.test_utils import user_stub, setup_required_data
 
-import unittest
+class ClientTests(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        # OBS: Using cl instead of client to evade conflicts with Client class
+        self.cl = Cl()
+        setup_required_data()
 
+    def test_client_index_should_redirect_unlogged_user(self):
+        req = self.cl.get(reverse('clients_index'))
+        self.assertEqual(req.status_code, 302)
 
-class ClientModelTest(unittest.TestCase):
+    def test_client_index_should_work_for_logged_user(self):
+        # user creation and login
+        self.cl.post(reverse('registration-register'), user_stub())
+        self.cl.login(
+            username=user_stub()["username"],
+            password=user_stub()["password1"]
+        )
+        user = User.objects.get(username=user_stub()["username"])
+        user.is_superuser = True
+        user.save()
+        req = self.cl.get(reverse('clients_index'))
+        self.assertEqual(req.status_code, 200)
+
+class ClientModelTest(TestCase):
     def setUp(self):
         #organization = Organization(name='Organizacao Teste',short_name='OT')
         person = Person()
